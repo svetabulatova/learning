9715335a7254a5630db3a9c6b4d154ee9049b83d
Sveta_Bulatova <sveta.bulatova@mail.ru>
2022-08-07 15:32:38 +0300

    добавление файлов задачи

---
+++ задача 1/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая на вход принимает число, 
+//а на выход выдает его квадрат(число, умноженное само на себя.)
+
+Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
